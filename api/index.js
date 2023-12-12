04d0e0b5d76003e81f8e76fada960d73cb07d69c
Ahasanul Rabbi Khan <me.ahsanul01@gmail.com>
2023-12-12 14:31:02 +0100

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
-import express from 'express'
+import express from 'express' ;
+import mongoose from 'mongoose';
+import dotenv from 'dotenv' ;
+dotenv.config();
+
+
+try {
+  await mongoose.connect(process.env.MONGO_URI)
+  console.log('Connected to database');
+} catch (error) {
+  handleError(error)
+}
+
 
 const app = express();
 app.listen(3000, () => {
